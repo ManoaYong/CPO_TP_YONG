ff1a7aa7ace25524d0418028b1b68e22e004a4a3
Manoa Yong <manoa.yong@epfedu.fr>
2024-09-23 18:55:30 +0200

    Création d'un nouveau projet versionné

---
+++ TP1_guessMyNumber_YONG/src/tp1_guessmynumber_yong/TP1_guessMyNumber_YONG.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_yong;
+
+import java.util.Random;
+
+/**
+ *
+ * @author manoa
+ */
+public class TP1_guessMyNumber_YONG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Random generateurAleat = new Random();
+    }
+    
+}
